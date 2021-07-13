e24ce95d9d3172d3cea77502212642fb2dfab844
lohit valavala <61038912+vlkumar-859@users.noreply.github.com>
2021-07-13 19:36:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 
-var site = "https://dog.ceo/api/breeds/image/random";
+var site1 = "https://dog.ceo/api/breed/";
+var breed = "";
 
 function fetch(){
+    var breed = document.getElementById("breed").value;
+    console.log(breed);
     var xhttp = new XMLHttpRequest();
 
     xhttp.onload = function(){
         var res = xhttp.response;
         res = JSON.parse(res);
-        document.getElementById("txt").innerHTML = "";
         document.getElementById("res-img").src = res.message;
+        console.log(res);
     }
 
-    xhttp.open("GET",site);
+    xhttp.open("GET","https://dog.ceo/api/breed/"+breed+"/images/random");
+   //xhttp.open("GET","https://dog.ceo/api/breeds/image/random");
     xhttp.send();
 }
