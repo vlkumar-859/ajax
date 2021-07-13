14297a571844ee7c1bbeda51b82522a5f9cec7f9
lohit valavala <61038912+vlkumar-859@users.noreply.github.com>
2021-07-13 13:31:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var site = "https://dog.ceo/api/breeds/image/random";
+
+function fetch(){
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onload = function(){
+        var res = xhttp.response;
+        res = JSON.parse(res);
+        document.getElementById("res-img").src = res.message;
+    }
+
+    xhttp.open("GET",site);
+    xhttp.send();
+}
